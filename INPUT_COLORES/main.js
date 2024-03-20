8c5a0d7a5bfb2dbbe74a0e5a9ba1e1f06579f0b6
franciscofarrando <121203370+franciscofarrando@users.noreply.github.com>
2024-03-20 14:46:02 +0100

    terminado

--- INPUT_COLORES/main.js
+++ INPUT_COLORES/main.js
@@ -1,3 +1,14 @@
+const coloresFondo = ["blue", "red", "green", "yellow", "white"];
+let i = 0;
+
+
+
 function cambiarColores(){
- document.getElementById("backColor").style.backgroundColor = "red";
+    const backColor = document.getElementById("backColor");
+    const inputText = document.getElementById("colores").value;
+    if (inputText !== ""){
+        backColor.style.backgroundColor = coloresFondo[i];
+        i = (i + 1) % coloresFondo.length;
+    }
+
 }
